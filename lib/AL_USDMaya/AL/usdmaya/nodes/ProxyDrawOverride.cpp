Autodesk/maya-usd

commit 321be29175a43aa270bb9d50387ac335c0d483d0

set ProxyDrawOverride's isAlwaysDirty to false
This makes it so that prepareForDraw is only called if the shape
actually needs to be updated, which should mean faster performance
for situations where things don't change - tumbling the
viewport being an important example

required a fix for updating viewport when a transform moves

lib/AL_USDMaya/AL/usdmaya/nodes/ProxyDrawOverride.cpp
@@ -41,7 +41,7 @@ public:
 
   // Constructor to use when shape is drawn but no bounding box.
   RenderUserData()
-    : MUserData(true)
+    : MUserData(false)
     {}
 
   // Make sure everything gets freed!
@@ -63,7 +63,7 @@ MString ProxyDrawOverride::kDrawRegistrantId("pxrUsd");
 //----------------------------------------------------------------------------------------------------------------------
 ProxyDrawOverride::ProxyDrawOverride(const MObject& obj)
 #if MAYA_API_VERSION >= 201700
-  : MHWRender::MPxDrawOverride(obj, draw, true)
+  : MHWRender::MPxDrawOverride(obj, draw, false)
 #else
   : MHWRender::MPxDrawOverride(obj, draw)
 #endif

lib/AL_USDMaya/AL/usdmaya/nodes/ProxyShape.cpp
@@ -452,6 +452,37 @@ void ProxyShape::constructGLImagingEngine()
 //----------------------------------------------------------------------------------------------------------------------
 MStatus ProxyShape::setDependentsDirty(const MPlug& plugBeingDirtied, MPlugArray& plugs)
 {
+  // I thought that, if your ProxyDrawOverride is set to not always be dirty,
+  // prepareForDraw would automatically be triggered whenever any attribute
+  // that was marked as affectsAppearance was dirtied; however, this is not
+  // the case, so we mark the draw geo dirty ourselves.
+  {
+    MStatus status;
+    MFnAttribute attr(plugBeingDirtied, &status);
+    if (!status)
+    {
+      if (plugBeingDirtied.isNull())
+      {
+        TF_CODING_ERROR(TfStringPrintf(
+            "ProxyShape setInternalValue given invalid plug - shape: %s",
+            name().asChar()));
+        }
+      else
+      {
+        TF_CODING_ERROR(TfStringPrintf(
+            "Unable to retrieve attribute from plug: %s",
+            plugBeingDirtied.name().asChar()));
+      }
+    }
+    else
+    {
+      if (attr.affectsAppearance())
+      {
+        MHWRender::MRenderer::setGeometryDrawDirty(thisMObject());
+      }
+    }
+  }
+
   if(plugBeingDirtied == m_time || plugBeingDirtied == m_timeOffset || plugBeingDirtied == m_timeScalar)
   {
     plugs.append(outTimePlug());
@@ -1820,6 +1851,7 @@ MStatus ProxyShape::computeOutputTime(const MPlug& plug, MDataBlock& dataBlock, 
 MStatus ProxyShape::compute(const MPlug& plug, MDataBlock& dataBlock)
 {
   TF_DEBUG(ALUSDMAYA_EVALUATION).Msg("ProxyShape::compute %s\n", plug.name().asChar());
+
   MTime currentTime;
   if(plug == m_outTime)
   {

lib/AL_USDMaya/AL/usdmaya/nodes/Transform.cpp
@@ -18,6 +18,7 @@
 #include "AL/usdmaya/DebugCodes.h"
 #include "AL/usdmaya/nodes/Transform.h"
 #include "AL/usdmaya/nodes/TransformationMatrix.h"
+#include "AL/usdmaya/nodes/ProxyShape.h"
 
 #include "maya/MBoundingBox.h"
 #include "maya/MDataBlock.h"
@@ -256,6 +257,34 @@ MBoundingBox Transform::boundingBox() const
     return MBoundingBox(minBound, maxBound);
   }
   return MPxTransform::boundingBox();
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+MStatus Transform::connectionMade(const MPlug& plug, const MPlug& otherPlug, bool asSrc)
+{
+  if(!asSrc && plug == m_inStageData)
+  {
+    MFnDependencyNode otherNode(otherPlug.node());
+    if (otherNode.typeId() == ProxyShape::kTypeId)
+    {
+      proxyShapeHandle = otherPlug.node();
+    }
+  }
+  return MPxTransform::connectionMade(plug, otherPlug, asSrc);
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+MStatus Transform::connectionBroken(const MPlug& plug, const MPlug& otherPlug, bool asSrc)
+{
+  if(!asSrc && plug == m_inStageData)
+  {
+    MFnDependencyNode otherNode(otherPlug.node());
+    if (otherNode.typeId() == ProxyShape::kTypeId)
+    {
+      proxyShapeHandle = MObject();
+    }
+  }
+  return MPxTransform::connectionBroken(plug, otherPlug, asSrc);
 }
 
 //----------------------------------------------------------------------------------------------------------------------

lib/AL_USDMaya/AL/usdmaya/nodes/Transform.h
@@ -19,6 +19,7 @@
 #include "AL/maya/utils/NodeHelper.h"
 #include "AL/usdmaya/utils/ForwardDeclares.h"
 #include "AL/maya/utils/MayaHelperMacros.h"
+#include "maya/MObjectHandle.h"
 #include "maya/MPxTransform.h"
 
 
@@ -124,6 +125,9 @@ public:
   MPxNode::SchedulingType schedulingType() const override
     { return kParallel; }
 
+  inline const MObject getProxyShape() const
+    { return proxyShapeHandle.object(); }
+
 private:
 
   //--------------------------------------------------------------------------------------------------------------------
@@ -135,6 +139,8 @@ private:
   MStatus compute(const MPlug &plug, MDataBlock &datablock) override;
   void postConstructor() override;
   MBoundingBox boundingBox() const override;
+  MStatus connectionMade(const MPlug& plug, const MPlug& otherPlug, bool asSrc) override;
+  MStatus connectionBroken(const MPlug& plug, const MPlug& otherPlug, bool asSrc) override;
   bool isBounded() const override
     { return true; }
   bool treatAsTransform() const override
@@ -220,6 +226,8 @@ private:
   /// \var    static MObject outTime();
   /// \brief  access the outTime attribute handle
   /// \return the outTime attribute
+
+  MObjectHandle proxyShapeHandle;
 };
 
 //----------------------------------------------------------------------------------------------------------------------

lib/AL_USDMaya/AL/usdmaya/nodes/TransformationMatrix.cpp
@@ -14,13 +14,14 @@
 // limitations under the License.
 //
 #include "AL/maya/utils/CommandGuiHelper.h"
+#include "AL/usdmaya/utils/AttributeType.h"
 #include "AL/usdmaya/TypeIDs.h"
 #include "AL/usdmaya/DebugCodes.h"
 #include "AL/usdmaya/nodes/Transform.h"
 #include "AL/usdmaya/nodes/TransformationMatrix.h"
 
 #include "maya/MFileIO.h"
-#include "AL/usdmaya/utils/AttributeType.h"
+#include "maya/MViewport2Renderer.h"
 #include "AL/usdmaya/utils/Utils.h"
 
 PXR_NAMESPACE_USING_DIRECTIVE
@@ -1887,6 +1888,23 @@ void TransformationMatrix::pushToPrim()
       {
       }
       break;
+    }
+  }
+
+  // Anytime we update the xform, we need to tell the proxy shape that it
+  // needs to redraw itself
+  if (!m_transformNode.isNull())
+  {
+    MStatus status;
+    MFnDependencyNode mfn(m_transformNode, &status);
+    if (status && mfn.typeId() == Transform::kTypeId)
+    {
+      auto xform = static_cast<Transform*>(mfn.userNode());
+      MObject proxy = xform->getProxyShape();
+      if (!proxy.isNull())
+      {
+        MHWRender::MRenderer::setGeometryDrawDirty(proxy);
+      }
     }
   }
 }

AbeWJS/AL_USDMaya

commit 56e84bde2f80ca6ba223675271d8813819b44f6a

Temporary restore alwaysDirty ProxyDrawOrride
(cherry picked from commit 6060036b78539b9b39c4b305c742e56571614e94)

MAYA-95165 : Crash when duplicating USD objects

   The regression was introduced by the commit https://git.autodesk.com/autodesk-forks/AL_USDMaya/commit/321be29175a43aa270bb9d50387ac335c0d483d0
   To avoid that problem we cherry-picked the correction from the last
   updates in the develop branch.

lib/AL_USDMaya/AL/usdmaya/nodes/ProxyDrawOverride.cpp
@@ -75,7 +75,7 @@ MUint64 ProxyDrawOverride::s_lastRefreshFrameStamp = 0;
 //----------------------------------------------------------------------------------------------------------------------
 ProxyDrawOverride::ProxyDrawOverride(const MObject& obj)
 #if MAYA_API_VERSION >= 201700
-  : MHWRender::MPxDrawOverride(obj, draw, false)
+  : MHWRender::MPxDrawOverride(obj, draw, true)
 #else
   : MHWRender::MPxDrawOverride(obj, draw)
 #endif